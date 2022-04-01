153d8e19281cc5fd057173408e579183ff8dea9e
jaclyntj <03081190016@student.uph.edu>
2022-04-01 18:11:19 +0700

    Update 01/04

--- resources/views/listMahasiswa.blade.php
+++ resources/views/listMahasiswa.blade.php
@@ -1,22 +1,21 @@
-<html>
-    <head>
-        <title>List Profil Mahasiswa</title>
-        <style>
-            table, th, td {
-                border: 1px solid black;
-                border-collapse: collapse;
-            }
-        </style>
-    </head>
-    <body>
-        <table>
-            <tr>
-            <th>Nama</th>
-            <th>Jurusan</th>
-            </tr>
-            @foreach ($mhs as $detailMhs)
-            <tr><td>   {{$detailMhs['nama']}} </td><td> {{$detailMhs['jurusan']}} </tr>
-            @endforeach
-        </table>
-    </body>
-</html>
+@extends('template.mastertemplate')
+
+@section('title', 'List Profil Mahasiswa')
+
+@section('content')
+<style>
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+</style>
+<table>
+    <tr>
+        <th>Nama</th>
+        <th>Jurusan</th>
+    </tr>
+    @foreach ($mhs as $detailMhs)
+    <tr><td>   {{$detailMhs['nama']}} </td><td> {{$detailMhs['jurusan']}} </tr>
+    @endforeach
+</table>
+@endsection
